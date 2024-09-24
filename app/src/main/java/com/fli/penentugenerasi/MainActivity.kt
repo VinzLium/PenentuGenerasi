26be767013e4e39e49b6ad4f215f5343dfbfed3f
Fli <muharafli827@gmail.com>
2024-09-24 09:59:20 +0700

    initial commit

--- app/src/main/java/com/fli/penentugenerasi/MainActivity.kt
+++ app/src/main/java/com/fli/penentugenerasi/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.fli.penentugenerasi
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //mulai komponen
+        var edTglLahir = findViewById<EditText>(R.id.ed_tanggal_lahir)
+        var btnKlik = findViewById<Button>(R.id.btn_tglLahir)
+        var tvHasil = findViewById<TextView>(R.id.tv_hasil)
+
+        btnKlik.setOnClickListener {
+            val input = edTglLahir.text.toString().toInt()
+            val hasil = when (input.toInt()) {
+
+            }
+        }
     }
-}
